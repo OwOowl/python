5c8e58430ab86bdffc5429b674aa0c329bd0dbbd
OwOowl <ymh0052@naver.com>
2022-11-28 16:19:08 +0900

    ch03

---
+++ ch03/ex5.py
@@ -0,0 +1,14 @@
+
+#대소문자 구분
+#자바와 선언 문법은 같음
+
+boolVar = False
+intVar = 100
+floatVar = 123.45
+strVar1 = "반갑다 월드컵"
+strVar2 = '반갑다 월드컵2'
+print(strVar2)
+strVar2 = True
+print(strVar2)
+print(type(intVar))
+print(type(strVar1))
