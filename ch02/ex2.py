698bac5ba8176c1cda9f9b2e20e0e02c859f1295
OwOowl <ymh0052@naver.com>
2022-11-28 16:18:54 +0900

    ch02

---
+++ ch02/ex2.py
@@ -0,0 +1,13 @@
+#ex2.py
+
+a = input()
+b = input()
+result = a + b
+print(a, " + ", b,  " = ", result)
+#문자열은 계산 안됨
+result = a - b
+print(a, ' - ', b, ' = ', result)
+result = a * b
+print(a, " * ", b, " = ", result)
+result = a / b
+print(a, " / ", b, " = ", result)
---
+++ ch02/ex4.py
@@ -0,0 +1,16 @@
+#ex4.py
+
+import turtle
+
+#turtle.shape('turtle')
+#turtle.shape('classic')
+turtle.shape('triangle')
+
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(200)
+turtle.right(90)
+
+turtle.done()
