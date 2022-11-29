5bc152615921b0d5600759d779c1b40422b719c8
OwOowl <ymh0052@naver.com>
2022-11-29 16:32:10 +0900

    ch13

---
+++ ch13/ex1.py
@@ -0,0 +1,22 @@
+import pymysql  # pip install pymysql
+
+con, cur = None, None
+data1, data2, data3, data4 = '', '', '', ''
+sql = ''
+
+con = pymysql.connect(host='localhost', user='root', password='1234', db='mydb', charset='utf8')
+cur = con.cursor()
+
+while True:
+    data1 = input('사용자 ID ==> ')
+    if data1 == '':
+        break
+    data2 = input('사용자 이름 ==> ')
+    data3 = input('사용자 이메일 ==> ')
+    data4 = input('사용자 출생년도 ==> ')
+    sql = "INSERT INTO tblmember2 VALUES('" + data1 + "','" + data2 + "','" + data3 + "','" + data4 + "')"
+    cur.execute(sql)
+
+con.commit()
+con.close()
+
